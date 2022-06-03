2a8cbdaf31e196d5d9fba80a18c9f61bc00103af
Andrew Beauchemin <android@vt.edu>
2022-06-03 17:04:58 -0400

    added getUserList and addUser methods to the UserService

--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -1,4 +1,20 @@
 package com.revature.service;
 
+import com.revature.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+    private List<User> userList = new ArrayList<>();
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void addUser(User user){
+        userList.add(user);
+    }
+
+
 }
